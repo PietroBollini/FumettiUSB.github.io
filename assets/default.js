aef318680e5589c6d956a701682fadb9d3cb3ff5
Francesco Bollini <francesco.bollini@icloud.com>
2019-09-07 23:37:22 +0200

    Set up jekyll

---
+++ assets/default.js
@@ -0,0 +1,14 @@
+(function (window) {
+
+	//// TOC Toggling //////////////////////////////////////////////////////////////////////////////////////////////////
+
+	window.onhashchange=function () {
+		window.document.getElementById("toggle").checked=false;
+	};
+
+
+	//// browser-specific css selectors (eg html[data-useragent*='MSIE 10.0']) /////////////////////////////////////////
+
+	document.documentElement.setAttribute('data-useragent', navigator.userAgent);
+
+})(window);
